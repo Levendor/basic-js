936d7cec905fcd51a93a58217d6c4c95f55bf083
Levendor <myth_of_lavanda@mail.ru>
2020-10-05 22:47:47 +0300

    feat: solve cats, team and dating

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if (!Array.isArray(members)) return false;
+	let team = members.reduce(function (dreamTeam, member) {
+		return typeof(member) == 'string' ? dreamTeam + member.trim()[0] : dreamTeam
+	}, '');
+	return team.toUpperCase().split('').sort().join('');
 };
